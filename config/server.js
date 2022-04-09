38806c6991278bf5d1dac69cfea4375fc7a7bf4e
Tuguldur <aa402115@gmail.com>
2022-04-09 13:40:18 +0800

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6987a84bc13442496a446e50c347a451'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c3dbe785-29b5-4dcc-ab5c-ab63c7c73b2a'
+};
